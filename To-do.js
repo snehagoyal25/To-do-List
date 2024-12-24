28027cc33a794327390cfa1efd281730c55d1e62
sneha <goyalsneha995@gmail.com>
2024-12-24 16:13:54 +0530

    list

--- To-do.js
+++ To-do.js
@@ -1,5 +1,12 @@
 function addTodo(){
     const inputElement = document.querySelector("input");
     const value = inputElement.value;
-    console.log(value);
-}
+    // console.log(value);
+    
+    const newDivElement = document.createElement("div");
+    newDivElement.innerHTML = value + "<button>Delete</button>";
+    
+    document.querySelector("body").appendChild(newDivElement);
+    
+    }
+    
