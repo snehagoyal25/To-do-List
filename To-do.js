6044fe7e0675c66b07514351a8fbc178aa0b39ae
sneha <goyalsneha995@gmail.com>
2024-12-26 13:37:27 +0530

    list

--- To-do.js
+++ To-do.js
@@ -1,12 +1,30 @@
-function addTodo(){
+
+function deletetodo(element){
+    if(element){
+    element.parentNode.removeChild(element);
+    }
+ }
+
+
+ function addTodo(){
     const inputElement = document.querySelector("input");
     const value = inputElement.value;
-    // console.log(value);
     
-    const newDivElement = document.createElement("div");
-    newDivElement.innerHTML = value + "<button>Delete</button>";
-    
-    document.querySelector("body").appendChild(newDivElement);
-    
+    const spanEl = document.createElement("span");
+    const buttonEl = document.createElement("button");
+    spanEl.innerHTML = value;
+    buttonEl.innerHTML = "Delete";
+
+    const divEl = document.createElement("div");
+    divEl.appendChild(spanEl);
+    divEl.appendChild(buttonEl);
+
+
+    buttonEl.addEventListener('click', function() {
+       deletetodo(divEl);  
+  });
+   document.querySelector("body").appendChild(divEl);
+   inputElement.value="";
+ 
     }
-    
+     
